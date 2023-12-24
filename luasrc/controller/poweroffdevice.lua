53446180a5b6917d156cc2fc09fb49f9d77e2eac
cn778 <39241549+cn778@users.noreply.github.com>
2023-12-24 14:05:02 +0800

    Update poweroffdevice.lua

--- luasrc/controller/poweroffdevice.lua
+++ luasrc/controller/poweroffdevice.lua
@@ -3,11 +3,11 @@ module("luci.controller.poweroffdevice", package.seeall)
 
 
 function index() 
-        entry({"admin","system","poweroffdevice"},template("poweroffdevice/poweroffdevice"), _("PowerOff"), 92)
-	entry({"admin","system","poweroffdevice","call"},post("action_poweroff"))
+        entry({"admin","system","poweroffdevice"},template("poweroffdevice/poweroffdevice"), _("RebootEth0"), 92)
+	entry({"admin","system","poweroffdevice","call"},post("action_rebooteth0"))
 end
 
-function action_poweroff()
-      luci.sys.exec("/sbin/poweroff" )
-
+function action_rebooteth0()
+      luci.sys.exec("ifconfig eth0 down" )
+      luci.sys.exec("ifconfig eth0 up" )
 end
